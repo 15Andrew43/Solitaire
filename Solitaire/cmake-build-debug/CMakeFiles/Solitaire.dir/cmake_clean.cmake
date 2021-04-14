ae28ea42a769bd2f3bdb24f158ef026dd51c16c1
AndrewB <avborovets@gmail.com>
2021-04-14 20:26:08 +0300

    begin

---
+++ Solitaire/cmake-build-debug/CMakeFiles/Solitaire.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Solitaire.dir/main.cpp.o"
+  "Solitaire"
+  "Solitaire.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Solitaire.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
